073daa15da73a6cf8c5933d375a4563fa5eeb14d
Bhavik Thakkar <bhavik.t@lucideustech.com>
2019-01-09 18:26:10 +0530

    Added more and less buttons and completed a basic version of the burger app

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -8,7 +8,7 @@ class BurgerIngredient extends Component {
         let ingredient = null;
 
     switch (this.props.type) {
-        case ('bread-botton'):
+        case ('bread-bottom'):
             ingredient = <div className={classes.BreadBottom}></div>;
             break;
         case ('bread-top'):
