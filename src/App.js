178742ecb139eb6cd06bbc65069006801ba9659f
Bhavik Thakkar <thakkarb97@gmail.com>
2019-02-04 11:52:32 +0530

    Added container and action types for authentication

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/" component={BurgerBuilder}/>
+  class App extends Component {
+    render() {
+      return (
+        <div>
+          <Layout>
+            <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/orders" component={Orders} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/" component={BurgerBuilder} />
             </Switch>
-        </Layout>
-      </div>
-    );
+          </Layout>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
